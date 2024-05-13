5d5bbf41b48b4c9c34d3bbe0ffb238f651c1ac09
lickos <g.sgoutas@chios.local>
2024-05-13 20:33:25 +0300

    For delivery

--- ypoergasia_3/3_code_template.py
+++ ypoergasia_3/3_code_template.py
@@ -20,17 +20,17 @@ pd_dataframe = pd.DataFrame(climate_data)
 # Β1. Συνάρτηση προβολής κλιματικών δεδομένων
 def display_climate_info():
     # Υλοποίηση της συνάρτησης προβολής κλιματικών δεδομένων
-    print(pd_dataframe)
+    print('Κλιματικά Δεδομένα:\n' + str(pd_dataframe))
 
 # Β2. Συνάρτηση υπολογισμού περιγραφικών στατιστικών (descriptive statistics)
 def calculate_descriptive_statistics():
     # Υλοποίηση της συνάρτησης υπολογισμού περιγραφικών στατιστικών (descriptive statistics)
-    print(pd_dataframe.describe())
+    print('Περιγραφικά Στατιστικά:\n' + str(pd_dataframe.describe()))
 
 # Β3. Συνάρτηση υπολογισμού μέσης θερμοκρασίας
 def calculate_average_temp():
     # Υλοποίηση της συνάρτησης υπολογισμού μέσης θερμοκρασίας
-    print(pd_dataframe['Θερμοκρασία'].mean())
+    print('Μέση Θερμοκρασία: ' + str("%.2f" %pd_dataframe['Θερμοκρασία'].mean()))
 
 # Β4. Συνάρτηση εκτύπωσης ραβδογράμματος (bar plot) υγρασίας
 def plot_humidity():
@@ -46,10 +46,32 @@ def plot_humidity():
 # Γ. Κύρια συνάρτηση
 def main():
     # Υλοποίηση μενού επιλογών με βάση τις συναρτήσεις που έχετε δημιουργήσει
-    display_climate_info()
-    calculate_descriptive_statistics()
-    calculate_average_temp()
-    plot_humidity()
+    print('''
+Μενού Κλιματικών Δεδομένων:
+1. Προβολή κλιματικών δεδομένων
+2. Υπολογισμός περιγραφικών στατιστικών (descriptive statistics)
+3. Υπολογισμός μέσης θερμοκρασίας
+4. Εκτύπωση ραβδογράμματος υγρασίας
+5. Έξοδος
+          ''')
+    
+
+    while True: 
+        user_choice = input('Εισάγετε την επιλογή σας: ')
+        if user_choice == '1':
+            display_climate_info()
+        elif user_choice == '2':
+            calculate_descriptive_statistics()
+        elif user_choice == '3':
+            calculate_average_temp()
+        elif user_choice == '4':
+            plot_humidity()
+        elif user_choice == '5':
+            quit()
+        else:
+            print('Παρακαλώ εισάγετε έναν αριθμό από το 1 έως το 5')
+    
+    
 
 if __name__ == "__main__":
     main()
